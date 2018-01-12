a93f3be3c079cb7a7deb4449e29a9591d448a19b
Denver Dsouza <dnvr.dsz@gmail.com>
2018-01-12 22:12:45 +0530

    Reactive Buttons

--- wpl/selfstudy/js/web.js
+++ wpl/selfstudy/js/web.js
@@ -1,8 +1,8 @@
 a = 4;
 setInterval(function () {
-  console.log('Woker Message');
+  // console.log('Woker Message');
   a=a+2;
   var workerResult=a;
-  console.log('Posting message back to main script');
+  // console.log('Posting message back to main script');
   postMessage(workerResult);
-},5000)
+},500)
