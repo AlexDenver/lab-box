1b3ef07ace3a9839774188a0a4265c248bb9f2c3
Alex Denver <dnvr.dsz@gmail.com>
2018-04-04 16:28:55 +0530

    Project Update

--- jsf/taskman/js/app.js
+++ jsf/taskman/js/app.js
@@ -1,45 +1,41 @@
-angular.module('taskman',[])
-	.controller('newTask',function($scope) {
-		
+angular.module('taskman',['angular-storage'])
+	.controller('newTask',function($scope,store) {
+		$scope.tasks = ['1'];
+		var json = (store.get('tasks'));
+		console.log(json);
+		if(json == null || json.length==0)
+			$scope.tasks = new Array();
+		else
+			$scope.tasks=json;
+		// store.remove('tasks');
+		// console.log(store);
+
+		$scope.create = function(body,color,typex) {
+			$scope.tasks.push({
+				no: $scope.tasks.length,
+				type: typex,
+				body: body,
+				color: color
+			});
+			
+			json = $scope.tasks;
+			// if(json.length==0 || json == null)
+			// else
+			// json.push($scope.tasks);
+
+			store.set('tasks',(json));
+		};
 	})
-	.controller('taskView',function($scope) {
-		$scope.tasks = [{
-			no: 0,
-			type: 'text',
-			body: 'You can also get the latest release using NPM. This release contains source files as well as the compiled CSS and JavaScript files.',
-			color: 'blue'
-		},{
-			no: 1,
-			type: 'list',
-			body: [{
-				text: 'Update Code',
-				done: false
-			},{
-				text: 'Commit Code',
-				done: false
-			},{
-				text: 'Push Code',
-				done: false
-			}],
-			color: 'blue'
-		},{
-			no: 2,
-			type: 'list',
-			body: [{
-				text: 'Update Code',
-				done: false
-			},{
-				text: 'Commit Code',
-				done: false
-			},{
-				text: 'Push Code',
-				done: false
-			}],
-			color: 'blue'
-		}];
+	.controller('taskView',function($scope,store) {
+		$scope.tasks = store.get('tasks');
+		// console.log(store.get('tasks'))
+		$scope.deleteTask=function(i) {
+			console.log("he");
+			$scope.tasks.splice(i,1);
+		};
+
+
 		$scope.markTask = function(taskIndex,bodyIndex) {
 			$scope.tasks[taskIndex].body[bodyIndex].done=!$scope.tasks[taskIndex].body[bodyIndex].done;
-			console.log($scope.tasks[taskIndex].body[bodyIndex].done);
-			console.log("Reached Mark Task");
 		};
 	})
