0e7218945e6ef1a66fe3ea98b7a6628929f4aa18
Alex Denver <dnvr.dsz@gmail.com>
2018-04-19 12:57:54 +0530

    PLC TUT Update

--- cpp/ovlTwoRel.cpp
+++ cpp/ovlTwoRel.cpp
@@ -1,22 +1,40 @@
 #include<iostream>
+#include<stdlib.h>
 using namespace std;
 class relOp{
 	private:
 		int a;
 	public: 
-x		relOp(int x){
+		relOp(int x){
 			a=x;
 		}
-		bool operator>(relOp rhs){
+		bool operator >(relOp rhs){
 			return (a > rhs.a);
 		}	
-		bool operator<(relOp rhs){
+		bool operator <(relOp rhs){
 			return (a < rhs.a);
 		}	
+		void* operator new(size_t x){
+			cout<<"New Overloaded\n";
+			// cout<<sizeof(size_t)<<"\n";
+			return ::new relOp(x);
+		}
+		void operator delete(void * p){
+			cout<< "Overloading delete operator " << endl;
+			cout<<p<<endl;	
+			free(p);
+	    }
 };
 
 int main(){
-	relOp num1(10),num2(20);
+	relOp num1(10) ,num2(20);
+	relOp *a = new relOp(5);
+	relOp *addr = new relOp(3);
+		cout<<"\nAddress1: "<<a<<endl;	
+		delete(a);
+		cout<<"\nAddress2: "<<addr<<endl;	
+		delete(addr);
+	cout<<"Num1 Deleted\n";
 	if(num1>num2)
 		cout<<"A is Greater than B";
 	else
