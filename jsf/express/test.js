15ed3857bd01fbeeb1d3018b07efe8fb38be738b
Alex Denver <dnvr.dsz@gmail.com>
2018-05-14 16:35:03 +0530

    JSF Lab 13 Added

--- jsf/express/test.js
+++ jsf/express/test.js
@@ -1,8 +1,39 @@
 app = require('express')();
-http = require('http');
 //console.log(app);
+path    = require("path");
+var bodyParser = require("body-parser");
+app.use(bodyParser());
 
-app.get('/', function (req, res,next) {
-  res.send('Hello from A!');
-  next();
-}).listen(1090)
+app.get('/', function (req, res) {
+  res.sendfile('index.html');
+})
+
+app.get('/home', function (req, res) {
+  res.send('Welcome Home!');  
+})
+
+app.get('/About', function (req, res) {
+  about = "Express.js, or simply Express, is a web application framework for Node.js, released as free and open-source software under the MIT License. It is designed for building web applications and APIs. It has been called the de facto standard server framework for Node.js."
+  //console.log(res);
+  res.send(about);  
+})
+
+app.post('/form', function (req, res) {  	
+	res.type('html');
+	// res.send(req.body);
+	if(req.body.usn=='user' && req.body.pwd=='pass'){
+		if(req.body.login)
+				res.send("<h1>Login Successful");
+		else
+				res.send("<script>alert('Login Successful');</script>");		
+	}else{
+		if(req.body.login)
+				res.send("<h1>Login Failed");							
+		else
+				res.send("<script>alert('Login Failed');</script>");			
+	}
+
+})
+
+.listen(1090);
+
