c050c956690c1dd88c11486f3d19ecf591cc945a
Alex Denver <dnvr.dsz@gmail.com>
2018-05-21 16:31:46 +0530

    JSF Lab 15 Added

--- jsf/express/test.js
+++ jsf/express/test.js
@@ -1,20 +1,19 @@
 express = require('express');
 app = express();
 var fs = require("fs")
+var jade = require('jade');
+var engines = require('consolidate');
 //console.log(app);
 path    = require("path");
 var bodyParser = require("body-parser");
 app.use(bodyParser());
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname+"/static"));
 
 app.get('/', function (req, res) {
   res.sendfile('index.html');
 })
 
-app.get('/home', function (req, res) {
-  res.send('Welcome Home!');  
-})
 
 app.get('/about', function (req, res) {
   about = "Express.js, or simply Express, is a web application framework for Node.js, released as free and open-source software under the MIT License. It is designed for building web applications and APIs. It has been called the de facto standard server framework for Node.js."
@@ -39,7 +38,40 @@ app.post('/form', function (req, res) {
 		else
 				res.send("<script>alert('Login Failed');</script>");
 	}
+})
+
+app.set('view engine', 'jade')
+
+app.get('/home',function(req,res) {
+	res.render('home', {title: "My Page"});
+})
+app.get('/login',function(req,res) {
+	res.render('form', {title: "My Page"});
+})
+app.get('/login/:mid([0-9a-z]{3,8})',function(req,res) {
+	res.render('form', {title: "My Page",myid:req.params.mid});
+})
+app.post('/result',function(req,res) {
+
+	res.render('result', {name: req.body.name, email: req.body.email});
 
 })
-.listen(1090);
+
+app.set('view engine', 'jade')
+
+app.get('/download/:img',function(req,res) {
+	res.download(__dirname+"/static/"+req.params.img);
+})
+
+app.engine('html', engines.swig);
+app.set('view engine', 'html');
+app.post('/html',function(req,res) {
+
+	res.render('htmlTemplate', {name: req.body.name, email: req.body.email});
+
+})
+.listen(1090,function(err){
+	if(err) throw err;
+	console.log("Listening on Port http://localhost:1090");
+});
 
