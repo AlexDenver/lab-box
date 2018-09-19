32e95e504296a9cfc7cdc12fc086c74681d03bd4
Alex Denver <dnvr.dsz@gmail.com>
2018-09-19 16:26:29 +0530

    PA Lab 2 Added

---
+++ pa/lab1.r
@@ -0,0 +1,38 @@
+version
+getwd()
+setwd("//home//mca//AZ//Lab1")
+V1 = c(2, "MCA2", 'b', 2.3)
+s = 1-1
+print(s)
+sqrt(4)
+
+ar1 <- c(5,2,1,4); 
+ar2 <- c(1,10,15,18); 
+y <- rep(4,5);
+ax = c(4, 5, 2, 5)
+z <- c(TRUE,FALSE,TRUE,TRUE); 
+w <- c("Marie","Betty","Peter");
+i = 0
+print(x)
+x = 10
+px = c(0, 10, 1, 8, 4, 7, 5, 3, 4, 6);
+py = c(0, 10, 1, 8, 4, 7, 5, 3, 4, 6);
+plot(px, py)
+
+px = seq(1,20,by=2)
+py = seq(1,30,by=3)
+plot(px,py)
+y = 2
+x = c(1,2,3,4)
+c(x,12)*2
+
+1:6 + 1
+1:4 + 3
+
+a = matrix(c(1,2,3,4,5,6), nrow = 3, ncol = 2, byrow = TRUE)
+x<-c(7.5,8.2,3.1,5.6,8.2,9.3,6.5,7.0,9.3,1.2,14.5,6.2)
+mean(x)
+var(x)
+summary(x)
+x[1:4]
+x[c(1,3,4)]
