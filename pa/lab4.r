60708919c2c944c84d915f17431f2a5bb569db75
Alex Denver <dnvr.dsz@gmail.com>
2018-10-10 16:20:00 +0530

    Git Ignore Update

--- pa/lab4.r
+++ pa/lab4.r
@@ -44,3 +44,17 @@ length(z)
 tele_data = read.csv("./Missing_Value_Telecom.csv")
 is.na(tele_data)
 removed_na = na.omit(tele_data)
+
+
+#FILLING MISSING DATA WITH MEAN/MEDIAN/MAX/MIN
+cmusage = mean(tele_data$Current.Month.s.Usage, na.rm = TRUE)
+l3musage = mean(tele_data$Last.3.Month.s.Usage, na.rm = TRUE)
+avgrchg = mean(tele_data$Average.Recharge, na.rm = TRUE)
+
+tele_data$Current.Month.s.Usage = cmusage
+tele_data$Current.Month.s.Usage[is.na(tele_data$Current.Month.s.Usage)] = cmusage
+tele_data$Last.3.Month.s.Usage[is.na(tele_data$Last.3.Month.s.Usage)] = l3musage
+tele_data$Average.Recharge[is.na(tele_data$Average.Recharge)] = avgrchg
+
+
+
