2aa3458af7918789b11845a493653a57a7acde0b
Alex Denver <dnvr.dsz@gmail.com>
2018-10-23 16:29:40 +0530

    PA Lab 4 Added

--- pa/lab4.r
+++ pa/lab4.r
@@ -1,61 +1,27 @@
-data()
-?mtcars
-names(mtcars)
-attach(mtcars)
+a = (1:10)
+b = (20:1)
+print (c(1:20,20:1))
 
-cov(mpg, hp)
-cor(mpg, carb)
+d = c(4,6,3)
 
-cor(mpg, mtcars$n)
-print(class(mtcars$n));
-x = names(mtcars[, 2:ncol(mtcars)]);
-print(x)
+data = read.csv('./newData.csv')
+plot(data$Distance, data$Stretch ) #, xlab = "Distance", ylab = "Stretch")
 
-z = c(1:5)
-length(z)
-
-for(n in x){
-  print(cat("mpg &",n,cov(mpg,mtcars[[n]])));
-}
-
-
-class()
+rep(c(4,6,3),10)
 
 
 
-cov(mpg, cyl)
-cor(mpg, cyl)
+c( rep(c(4,6,3),10), 4)
+c( rep(4,10), rep(6, 20), rep(3,30) )
 
-cov(cyl, disp)
-cor(cyl, disp)
+attach(cars)
 
+cor(cars$speed, cars$dist)
+cor(cars$speed, cars$dist)
 
+boxplot(cars, horizontal = TRUE)
 
-
-
-z=c(1:5,NA)
-is.na(z)
-
-
-z=
-length(z)
-
-# REMOVING EMPTY DATA COMPLETELY
-tele_data = read.csv("./Missing_Value_Telecom.csv")
-is.na(tele_data)
-removed_na = na.omit(tele_data)
-
-
-#FILLING MISSING DATA WITH MEAN/MEDIAN/MAX/MIN
-cmusage = mean(tele_data$Current.Month.s.Usage, na.rm = TRUE)
-l3musage = mean(tele_data$Last.3.Month.s.Usage, na.rm = TRUE)
-avgrchg = mean(tele_data$Average.Recharge, na.rm = TRUE)
-
-tele_data$Current.Month.s.Usage = cmusage
-tele_data$Current.Month.s.Usage[is.na(tele_data$Current.Month.s.Usage)] = cmusage
-tele_data$Last.3.Month.s.Usage[is.na(tele_data$Last.3.Month.s.Usage)] = l3musage
-tele_data$Average.Recharge[is.na(tele_data$Average.Recharge)] = avgrchg
-#TABLE_NAME $ COL_NAME [COL_NAME  TEST CONDITION] = NEW VALUE
-
-x = c(1,2,3,4,NA,NA)
-sum(is.na(x))
+M1 = lm(dist~speed)
+M1
+predict.lm(M1, data.frame(speed=50))
+?predict
